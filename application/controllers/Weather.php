c8f1ff822c4e63c0d480245425bac019a136488e
Carbyn <carbyn.wu@gmail.com>
2018-05-07 14:00:26 +0800

    add location in resp

--- application/controllers/Weather.php
+++ application/controllers/Weather.php
@@ -7,6 +7,7 @@ class WeatherController extends Yaf\Controller_Abstract {
 
 		$location = \Explorer\Lbsyun::geocoder($longitude, $latitude);
 		$weather = \Explorer\Lbsyun::fetch($location);
+		$weather['location'] = $location;
 		$weather['tips'] = '你丫不用穿了，裸奔吧！';
 		$weather['timestamp'] = time();
 		$weather['is_night'] = date('H') > 18 ? 1 : 0;
